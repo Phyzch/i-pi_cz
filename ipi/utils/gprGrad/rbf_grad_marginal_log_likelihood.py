16d8b8f419a1b7b14b64f7e772e2de09eb841556
Chenghao Zhang <chenghao.zhang@pnnl.gov>
2024-09-17 11:03:40 -0700

    code reports error when making prediction using GPR

---
+++ ipi/utils/gprGrad/rbf_grad_marginal_log_likelihood.py
@@ -0,0 +1,63 @@
+import gpytorch
+from gpytorch.mlls import ExactMarginalLogLikelihood
+from gpytorch.distributions import MultivariateNormal, MultitaskMultivariateNormal
+import torch
+from torch import Tensor
+import math 
+
+class RBFGradMarginalLogLikelihood(ExactMarginalLogLikelihood):
+    """
+    simple extension of ExactMarginalLogLikelihood class.
+    We change the __forward__() function since num_data is different for our case (not function_dist.event_shape.numel() in ExactMarginalLogLikelihood)
+    """
+
+    def __init__(self, likelihood, model):
+        super(RBFGradMarginalLogLikelihood, self).__init__(likelihood, model)
+
+    def forward(self, function_dist, target, *params):
+        r"""
+        Computes the MLL given :math:`p(\mathbf f)` and :math:`\mathbf y`.
+
+        :param ~gpytorch.distributions.MultivariateNormal function_dist: :math:`p(\mathbf f)`
+            the outputs of the latent function (the :obj:`gpytorch.models.ExactGP`)
+        :param torch.Tensor target: :math:`\mathbf y` The target values
+        :rtype: torch.Tensor
+        :return: Exact MLL. Output shape corresponds to batch shape of the model/input data.
+        """
+        if not isinstance(function_dist, MultivariateNormal):
+            raise RuntimeError("ExactMarginalLogLikelihood can only operate on Gaussian random variables")
+
+        # Get the log prob of the marginal distribution
+        output = self.likelihood(function_dist, *params)  # output is the multitaskmultivariate normal distribution with noise var add to variance.
+        res = self.log_prob_likelihood(output, target)
+        # res1 = output.log_prob(target)   # old code
+        res = self._add_other_terms(res, params)
+
+        # Scale by the amount of data we have
+        num_data = function_dist.event_shape.numel()
+        return res.div_(num_data)
+    
+    def log_prob_likelihood(self, normal_dist: MultitaskMultivariateNormal, value: Tensor, singular_value_cutoff = pow(10.0, -8)):
+        """
+        compute the log probability of observable (target).
+        Perform the pseudo-inverse when the covariance matrix is ill-conditioned.
+        See log_prob function in multitask_multivariate_normal.py & multivariate_normal.py in gpytorch.distributions
+        log(p) = -1/2 ( y^t (K + sigma^2 I)^-1 y + log|K + sigma^2 I| + n * log(2* pi) )
+        """
+        if not normal_dist._interleaved:
+            new_shape = value.shape[:-2] + value.shape[:-3:-1]
+            value = value.view(new_shape).transpose(-1, -2).contiguous() 
+        value = value.reshape(*value.shape[:-2], -1)
+
+        mean, covar = normal_dist.loc, normal_dist.lazy_covariance_matrix
+        diff = value - mean 
+
+        # get log determinant and first part of quadratic form
+        covar_tensor = covar.to_dense() 
+        logdet = torch.logdet(covar_tensor) # log(|K + sigma^2 I|)
+        
+        pseudo_inverse_covar = torch.linalg.pinv(covar_tensor, atol= singular_value_cutoff) 
+        inv_quad = diff @ pseudo_inverse_covar @ diff # y^t (K+ sigma^2 I)^-1 y
+
+        res = -0.5 * sum([inv_quad, logdet, diff.size(-1) * math.log(2 * math.pi) ])
+        return res 
--- ipi/utils/gprGrad/rbf_grad_prediction_strategies.py
+++ ipi/utils/gprGrad/rbf_grad_prediction_strategies.py
@@ -9,6 +9,9 @@ from gpytorch.utils.memoize import (
     cached,
     clear_cache_hook,
 )
+from linear_operator import to_dense
+from gpytorch import settings 
+import functools
 
 class RBFGradPredictionStrategies(DefaultPredictionStrategy):
     def __init__(self, train_inputs, train_prior_dist, train_labels, likelihood, root=None, inv_root=None):
@@ -16,12 +19,62 @@ class RBFGradPredictionStrategies(DefaultPredictionStrategy):
     
     @property 
     @cached(name="mean_cache")
-    def mean_cache(self):
+    def mean_cache(self, singular_value_cutoff = pow(10.0, -8)):
         """
-        compute cache for prediction of the mean value. 
+        compute cache for the prediction of the mean value. 
         use pseudo-inverse (Moore-Penrose inverse) when the covariance matrix becomes ill-defined.
         """
         mvn = self.likelihood(self.train_prior_dist, self.train_inputs)
         train_mean, train_train_covar = mvn.loc, mvn.lazy_covariance_matrix  # covariance matrix of y(x) (likelihood) : K(X,X) + sigma^2 I
 
-        train_label_offset = (self.train_labels - train_mean).unsqueeze(-1)   # y
+        train_labels_offset = (self.train_labels - train_mean).unsqueeze(-1)   # y
+        
+        # pseudo-inverse the covariance matrix.
+        train_train_covar_tensor = train_train_covar.to_dense()
+        covar_eigval = torch.linalg.eigvals(train_train_covar_tensor)
+        covar_eigval_min = torch.min(torch.real(covar_eigval)) 
+        if covar_eigval_min < singular_value_cutoff:
+            # the covariance matrix is ill-conditioned. We should perform the pseudo-inverse 
+            covar_inverse = torch.linalg.pinv(train_train_covar_tensor, atol= singular_value_cutoff) # (K(X,X) + sigma^2 I)^-1 * y
+            mean_cache = (covar_inverse @ train_labels_offset).squeeze(-1) 
+        else:
+            mean_cache = train_train_covar.evaluate_kernel().solve(train_labels_offset).squeeze(-1)  # (K(X,X) + sigma^2 I)^-1 * y
+        
+        if settings.detach_test_caches.on():
+            mean_cache = mean_cache.detach()
+
+        if mean_cache.grad_fn is not None:
+            wrapper = functools.partial(clear_cache_hook, self)
+            functools.update_wrapper(wrapper, clear_cache_hook)
+            mean_cache.grad_fn.register_hook(wrapper)
+
+        return mean_cache 
+
+    @property
+    @cached(name="covar_cache")
+    def covar_cache(self, singular_value_cutoff = pow(10.0, -8)):
+        """
+        compute cache for the prediction of the covariance matrix. Which is (K(x,x) + sigma^2 I)^{-1/2}
+        use pseudo-inverse (Moore-Penrose inverse) when the covariance matrix becomes ill-conditioned
+        """
+        train_train_covar = self.lik_train_train_covar 
+
+        # pseudo-inverse the covariance matrix
+        train_train_covar_tensor = train_train_covar.to_dense()
+        covar_eigval = torch.linalg.eigvals(train_train_covar_tensor)
+        covar_eigval_min = torch.min(torch.real(covar_eigval)) 
+
+        if covar_eigval_min < singular_value_cutoff:
+            # the covariance matrix is ill-conditioned. We should perform the pseudo-inverse 
+            U, S ,Vh = torch.linalg.svd(train_train_covar_tensor)
+            nonzero_indices = (S > singular_value_cutoff).nonzero().squeeze(-1)
+            nonzero_s = S[nonzero_indices]
+            nonzero_u = torch.index_select(U, dim= 1, index= nonzero_indices)
+            nonzero_vh = torch.index_select(Vh, dim= 0, index= nonzero_indices)
+            nonzero_s_inv_root = 1/ torch.sqrt(nonzero_s)
+            nonzero_s_inv_root_matrix = torch.diag(nonzero_s_inv_root)
+            train_train_covar_inv_root = nonzero_u @ nonzero_s_inv_root_matrix @ nonzero_vh # (K(x,x) + sigma^2 I)^(-1/2)
+        else:
+            train_train_covar_inv_root = to_dense(train_train_covar.root_inv_decomposition().root)   # (K(x,x) + sigma^2 I)^(-1/2)
+
+        return train_train_covar_inv_root
