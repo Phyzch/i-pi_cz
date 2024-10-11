fix the criterion for gpr model to use pseudo-inverse as condition number of the covariance matrix.
use the original time step for projected velocity verlet algorithm for drifting.

ipi/engine/motion/neb_instanton_gpr.py
@@ -66,7 +66,7 @@ class MAPNEBGPRMover(Motion):
         tolerances={"gradient": 5e-3, 
                     "gradient_end_bead": 1e-2,
                     "action_forces_sum": 5e-3,
-                    "action": 1e-4},
+                    "action": 1e-3},
         energy_shift=0.00,
         FIRE={"tmax": 4.0, "tmin": 0.1, 
               "Ndelay": 5, "finc": 1.1, "fdec": 0.5, 
@@ -97,7 +97,7 @@ class MAPNEBGPRMover(Motion):
         gpr_absolute_force_error_criterion=0.002,
         gpr_trust_region=0.1,
         minimum_trust_region= 0.05,
-        distance_cutoff_for_training_data= 0.05,
+        distance_cutoff_for_training_data= 0.1,
         gpr_kernel_outputscale=np.zeros(0, float),
         gpr_kernel_lengthscale_ratio=np.zeros(0, float),
         gpr_noise_std={
@@ -125,6 +125,10 @@ class MAPNEBGPRMover(Motion):
               motion will be constrained or not. Defaults to False.
         """
         super(MAPNEBGPRMover, self).__init__(fixcom=fixcom, fixatoms=fixatoms)
+
+        if distance_cutoff_for_training_data > gpr_trust_region:
+            distance_cutoff_for_training_data = gpr_trust_region
+            print(f"readjust distance cutoff for rejecting training data to trust region value {gpr_trust_region}")
 
         # parameters to pass in from input.xml
         self.options = {}
@@ -1105,13 +1109,16 @@ class MAPNEBGPRMover(Motion):
 
         action, action_gradient_mean = self.actiongm(x_mscaled)
 
+        fdf0 = (action, action_gradient_mean)
+
+        time_step = self.optarrays["time_step"]
         x_mscaled, self.drift_velocity_mscaled, self.action, action_gradient_mean = \
             ipi.utils.nebinstool.projected_verlet(
                 x_mscaled,
                 self.drift_velocity_mscaled,
                 (action, action_gradient_mean),
                 self.actiongm,
-                self.time_step
+                time_step
             )
         
         self.x = x_mscaled / np.sqrt(

ipi/utils/gprGrad/rbf_grad_marginal_log_likelihood.py
@@ -57,10 +57,12 @@ class RBFGradMarginalLogLikelihood(ExactMarginalLogLikelihood):
         # get log determinant and first part of quadratic form
         covar_tensor = covar.to_dense() 
 
-        covar_eigval, _ = torch.lobpcg(covar_tensor, k= 1, largest= False)
+        # covar_eigval, _ = torch.lobpcg(covar_tensor, k= 1, largest= False)
+        covar_eigval = torch.linalg.eigvals(covar_tensor)
         covar_eigval_min = torch.min(torch.real(covar_eigval))
+        covar_eigval_max = torch.max(torch.real(covar_eigval))
 
-        if covar_eigval_min < singular_value_cutoff:
+        if abs(covar_eigval_min / covar_eigval_max) < singular_value_cutoff:
             # the covariance matrix is ill-conditioned. 
             logdet = torch.logdet(covar_tensor) # log(|K + sigma^2 I|)
             

ipi/utils/gprGrad/rbf_grad_prediction_strategies.py
@@ -32,9 +32,12 @@ class RBFGradPredictionStrategies(DefaultPredictionStrategy):
         # pseudo-inverse the covariance matrix.
         train_train_covar_tensor = train_train_covar.to_dense()
         # compute eigenvalues using lobpcg: faster for sparse matrix.
-        covar_eigval, _ = torch.lobpcg(train_train_covar_tensor, k=1, largest= False)
-        covar_eigval_min = torch.min(torch.real(covar_eigval)) 
-        if covar_eigval_min < singular_value_cutoff:
+        # covar_eigval, _ = torch.lobpcg(train_train_covar_tensor, k=1, largest= False)
+        covar_eigval = torch.linalg.eigvals(train_train_covar_tensor)
+        covar_eigval_min = torch.min(torch.real(covar_eigval))
+        covar_eigval_max = torch.max(torch.real(covar_eigval))
+
+        if abs(covar_eigval_min / covar_eigval_max) < singular_value_cutoff:
             # the covariance matrix is ill-conditioned. We should perform the pseudo-inverse 
             covar_inverse = torch.linalg.pinv(train_train_covar_tensor, atol= singular_value_cutoff) # (K(X,X) + sigma^2 I)^-1 * y
             mean_cache = (covar_inverse @ train_labels_offset).squeeze(-1) 
@@ -62,10 +65,12 @@ class RBFGradPredictionStrategies(DefaultPredictionStrategy):
 
         # pseudo-inverse the covariance matrix
         train_train_covar_tensor = train_train_covar.to_dense()
-        covar_eigval, _ = torch.lobpcg(train_train_covar_tensor, k= 1, largest= False)
-        covar_eigval_min = torch.min(torch.real(covar_eigval)) 
+        # covar_eigval, _ = torch.lobpcg(train_train_covar_tensor, k= 1, largest= False)
+        covar_eigval = torch.linalg.eigvals(train_train_covar_tensor)
+        covar_eigval_min = torch.min(torch.real(covar_eigval))
+        covar_eigval_max = torch.max(torch.real(covar_eigval))
 
-        if covar_eigval_min < singular_value_cutoff:
+        if abs(covar_eigval_min / covar_eigval_max) < singular_value_cutoff:
             # the covariance matrix is ill-conditioned. We should perform the pseudo-inverse 
             U, S ,Vh = torch.linalg.svd(train_train_covar_tensor)
             nonzero_indices = (S > singular_value_cutoff).nonzero().squeeze(-1)

ipi/utils/gprHessian/RBFHessian_prediction_strategy.py
@@ -122,9 +122,10 @@ class RBFHessianPredictionStrategy(DefaultPredictionStrategy):
         # pseudo-inverse the covariance matrix
         train_train_covar_tensor = train_train_covar.to_dense()
         covar_eigval = torch.linalg.eigvals(train_train_covar_tensor)
-        covar_eigval_min = torch.min(torch.real(covar_eigval)) 
+        covar_eigval_min = torch.min(torch.real(covar_eigval))
+        covar_eigval_max = torch.max(torch.real(covar_eigval))
 
-        if covar_eigval_min < singular_value_cutoff:
+        if abs(covar_eigval_min / covar_eigval_max) < singular_value_cutoff:
             # the covariance matrix is ill-conditioned. We should perform the pseudo-inverse 
             U, S ,Vh = torch.linalg.svd(train_train_covar_tensor)
             nonzero_indices = (S > singular_value_cutoff).nonzero().squeeze(-1)