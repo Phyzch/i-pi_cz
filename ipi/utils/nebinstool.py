fix get_hessian(). Now hessian component along fixed dofs is 0.

ipi/utils/nebinstool.py
@@ -356,7 +356,7 @@ def get_hessian(rp_beads, rp_forces, x0, natoms, nbeads=1,  fixdofs = [], d=0.00
 
     OUT    h       = physical hessian ( (natoms-len(fixatoms) )*3 , nbeads*( natoms-len(fixatoms) )*3)
     """
-
+    # TODO: Make the fixed_dofs part of hessian = 0.
     info(" @get_hessian: Computing hessian", verbosity.low)
     ii = natoms * 3
     activedof = np.delete(np.arange(ii), fixdofs)
@@ -417,6 +417,9 @@ def get_hessian(rp_beads, rp_forces, x0, natoms, nbeads=1,  fixdofs = [], d=0.00
 
             # COMBINE
             g = (g1 - g2) / (2 * d)
+            # Set hessian component along fixed dofs as 0.
+            g[:, fixdofs] = 0
+            
             h[j, :] = g.flatten()
 
             # save hessian temporary file (record hessian up to row j.)