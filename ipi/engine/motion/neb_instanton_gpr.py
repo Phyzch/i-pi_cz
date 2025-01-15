correct one small bug in hessian calculation in internal coordinate. We should transform g_q first then compute h_q in internal coordinate.

ipi/engine/motion/neb_instanton_gpr.py
@@ -546,11 +546,10 @@ class MAPNEBGPRMover(Motion):
         bead_index_at_transition_state = np.argmax(beads_pots)
         ref_x = dstrip(self.beads.q[bead_index_at_transition_state]).copy()
 
-        # FIXME: The best solution is to provide both the reactant and product state
-        # FIXME: as reference points to construct coordinate transformer.
         # Now, we just use the poiont with lowest energy at reactant and product side.
         ref_x_reactant = dstrip(self.beads.q[0]).copy() # coordinate at reactant side.
         ref_x_product = dstrip(self.beads.q[-1]).copy() # coordinate at product side
+
         ref_x_list = np.array([ref_x, ref_x_reactant, ref_x_product])
 
         # create coordinate_transformer, which handles the transformation from the Cartesian coordinate to internal coordinate.
@@ -566,8 +565,6 @@ class MAPNEBGPRMover(Motion):
             ref_x_list,
             names
         )
-
-        # TODO: output motion of each internal coordinate.
 
         # attach ab_initio potential to self.nebgm.ab_initio_pot and self.nebgm.ab_initio_force.
         # In the LI-NEB algorithm, when there is ab-initio potential & force data available, we will use that potential and force.
@@ -3570,7 +3567,6 @@ class RP_MAP(object):
                     ), "At least one data point in new_grad_data_index coincide with the one point that we have already computed grads.\
                         please double check new_grad_data_index entry in input.xml" 
             
-            # FIXME: add perturbation to the candidate training data point.
             # perturbed_new_x = ipi.utils.nebinstgprtool.perturb_training_point(candidate_grad_point_x,
             #                                                                   self.new_grad_data_index,
             #                                                                   self.gpr_hessian_model,

ipi/utils/hessfasttools.py
@@ -63,16 +63,23 @@ class SelectiveHessianCalculation:
               for reference, train_inputs_change: {train_inputs_change}")
         
         print(f"The cutoff value for determining rigid internal dofs is: {rigid_internal_dofs_cutoff}")
+        
+        
         self.rigid_internal_dofs = np.array(
             [
                 i for i in range(input_dim)
                 if train_inputs_change[i] < rigid_internal_dofs_cutoff
             ]
         )
+        # FIXME: Set rigid_internal_dofs = 0 first. Check the error from coordinate transformation & hessian calculation.
+        # self.rigid_internal_dofs = np.array([])
 
         print(f"@hessian calculation: rigid internal dofs: {self.rigid_internal_dofs}")
 
-        self.flexible_internal_dofs = np.delete(np.arange(input_dim), self.rigid_internal_dofs)
+        if len(self.rigid_internal_dofs) != 0:
+            self.flexible_internal_dofs = np.delete(np.arange(input_dim), self.rigid_internal_dofs)
+        else:
+            self.flexible_internal_dofs = np.arange(input_dim)
 
         print(f"@hessian calculation: flexible internal dofs: {self.flexible_internal_dofs}")
         self.rigid_hessian_component_bool = False 
@@ -110,25 +117,31 @@ class SelectiveHessianCalculation:
 
         # dx = inverse_Bq @ dq[np.newaxis, np.newaxis, :]
         dx = np.einsum('pmn,n->pm', inverse_Bq, dq)
-        
+
         # compute force:
         # PLUS
         x = np.copy(train_x1)
         x = x + dx 
+        x_plus = np.copy(x)
         beads.q[:] = x 
         g1 = - forces.f 
-
+        g1q = self.coordinate_transformer.transform_cartesian_gradient_to_internal_gradient(
+            x_plus,
+            g1
+        )
         # MINUS
         x = np.copy(train_x1)
         x = x - dx 
+        x_minus = np.copy(x)
         beads.q[:] = x 
         g2 = - forces.f 
-
+        g2q = self.coordinate_transformer.transform_cartesian_gradient_to_internal_gradient(
+            x_minus,
+            g2
+        )
         # COMBINE 
-        gx = (g1 - g2)/ (2 * d)
-        gq = self.coordinate_transformer.transform_cartesian_gradient_to_internal_gradient(
-            train_x1,
-            gx)
+        # transform each gradient into internal coordinate. Then compute hessian component 
+        gq = (g1q - g2q) / (2 * d)
         
         hess[:, index_q, :] = gq
         hess[:, :, index_q] = gq 

ipi/utils/nebinstgprtool.py
@@ -536,7 +536,6 @@ def read_hessian_data(prefix, ndofs):
 
     return hessian_index_list, hessian_data_list 
 
-
 def read_training_data_with_hessian(prefix):
     """
     read coordinate, potential V, force f and hessian h from training data
@@ -690,7 +689,7 @@ def compute_frobenius_norm(input_matrix: np.ndarray):
     elif len(np.shape(input_matrix)) == 3:
         matrix = input_matrix
     else:
-        raise "the shape of matrix for computing frobenius norm has to have 2 or 3 dimensions"
+        raise "the shape of matrix for computing frobenius norm has to have dimensions 2 or 3."
 
     matrix_transpose = np.transpose(matrix, (0, 2, 1))
     frobenius_norm = np.sqrt(