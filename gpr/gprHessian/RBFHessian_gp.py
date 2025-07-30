change covar func to single gpu squared exponential kernel. use a different interpolation for ring polymer position (fixed end beads at reactant and product) for tunneling splitting calculation.

gpr/gprHessian/RBFHessian_gp.py
@@ -309,15 +309,7 @@ class GPModelWithHessians(gpytorch.models.ExactGP):
             covar_module = covar_module + self.covar_module_component_list[i]
 
         # use multi-device kernel.
-        n_devices = torch.cuda.device_count() 
-        print("Planning to run on {} GPUs".format(n_devices))
-        output_device = torch.device("cuda:0")
-
-        self.covar_module = gpytorch.kernels.MultiDeviceKernel(
-            covar_module, 
-            device_ids= range(n_devices),
-            output_device= output_device
-        )
+        self.covar_module = covar_module 
 
     def _set_likelihood_noise_prior(
         self,

ipi/utils/nebinstool.py
@@ -171,7 +171,12 @@ def path_equal_distance_interpolation(neb_bead_q, interpolation_bead_number):
 
 
 def interpolate_ring_polymer_beads(
-    period, t_list, x_list, v_list, instanton_bead_number
+    period, 
+    t_list, 
+    x_list, 
+    v_list, 
+    instanton_bead_number,
+    cal_type= "rate"
 ):
     """
     interpolate the position of ring polymer beads along minmium action path.
@@ -186,12 +191,20 @@ def interpolate_ring_polymer_beads(
     :return: rp_t_list: time list for instanton ring-polymer
              rp_x_list: coordinate list for instanton ring-polymer
     """
-    rp_t_list = np.linspace(
-        0, period / 2, instanton_bead_number, endpoint=False
-    )  # i * beta * hhbar / (2N) : here i = 0, ..., N-1.
-    rp_t_list = rp_t_list + period / (
-        4 * instanton_bead_number
-    )  # (1/2 + i) * (beta * hbar / (2N)) : here i = 0, .., N-1
+    if cal_type == "rate":
+        rp_t_list = np.linspace(
+            0, period / 2, instanton_bead_number, endpoint=False
+        )  # i * beta * hhbar / (2N) : here i = 0, ..., N-1.
+        rp_t_list = rp_t_list + period / (
+            4 * instanton_bead_number
+        )  # (1/2 + i) * (beta * hbar / (2N)) : here i = 0, .., N-1
+    elif cal_type == "splitting":
+        # tunneling splitting calculation.
+        # linear ring polymer with fixed two end points.
+        rp_t_list = np.linspace(0, period/ 2, instanton_bead_number + 2, endpoint= True)
+        rp_t_list = rp_t_list[1:-1]
+    else:
+        raise ValueError("unrecognized cal_type. Must be rate or splitting.")
 
     print("imaginary time list for ring polymer: " + str(rp_t_list))
 

tools/py/Instanton_postproc.py
@@ -420,7 +420,7 @@ elif case == "instanton":
             h = np.add(h0, spring)
             if asr != "none":
                 print(
-                    "We are changing asr to none since we consider a fixed ended linear polimer for the post-processing"
+                    "We are changing asr to none since we consider a fixed ended linear polymer for the post-processing"
                 )
                 asr = "none"
     else: