fix error about fast hessian calculation in gpr_interface.py
fix the inconsistency between hessfasttools.py and gpr_hessian_tools.py.

gpr_script/gpr_interface.py
@@ -795,7 +795,7 @@ class GPRHessianMapper(object):
         self.gpr_covar_inverse_nugget = nebmover.optarrays["gpr_covar_inverse_nugget"]
 
 # ------- construct gpr hessian model --------------------
-    def construct_selective_hessian_calculator(self, candidate_hessian_point_x):
+    def construct_selective_hessian_calculator(self, candidate_hessian_point_x, grads_x):
         """
         initialize SelectiveHessianCalculation class.
         Use this function to only compute a few number of beads along rigid mode 
@@ -806,6 +806,7 @@ class GPRHessianMapper(object):
         """
         self.selective_hessian_calculator = ipi.utils.hessfasttools.SelectiveHessianCalculation(
             candidate_hessian_point_x,
+            grads_x,
             self.coordinate_transformer,
             self.gpr_rigid_internal_dofs_cutoff,
             self.cross_validation_bool
@@ -894,9 +895,11 @@ class GPRHessianMapper(object):
 
         if self.selective_hessian_bool:
             # initialize the selective hessian calculator to compute hessian along rigid modes.
-            self.construct_selective_hessian_calculator(candidate_hessian_point_x)
+            grads_x = -training_forces
+            self.construct_selective_hessian_calculator(candidate_hessian_point_x,
+                                                        grads_x)
             # update the hessian along the rigid mode.
-            hessian_data_list = self.rp_map.selective_hessian_calculator.update_hessian_rigid_modes(
+            hessian_data_list = self.selective_hessian_calculator.update_hessian_rigid_modes(
                 cartesian_coordinate_x[hessian_index_list],
                 training_forces[hessian_index_list],
                 hessian_data_list
@@ -1007,9 +1010,9 @@ class GPRHessianMapper(object):
 
         if self.selective_hessian_bool:
             # initialize the selective_hessian_calculator.
-            self.rp_map.construct_selective_hessian_calculator(candidate_hessian_point_x)
+            self.construct_selective_hessian_calculator(candidate_hessian_point_x, train_grads)
 
-            ref_hessians = self.rp_map.selective_hessian_calculator.get_hessian(
+            ref_hessians = self.selective_hessian_calculator.get_hessian(
                 new_beads,
                 new_forces,
                 np.copy(new_beads.q)
@@ -1202,7 +1205,7 @@ class GPRHessianMapper(object):
 
                 # compute ab initio hessians of new data points.
                 if self.selective_hessian_bool:
-                    new_hessians = self.rp_map.selective_hessian_calculator.get_hessian(
+                    new_hessians = self.selective_hessian_calculator.get_hessian(
                         new_beads,
                         new_forces,
                         np.copy(new_beads.q)

ipi/engine/motion/neb_instanton_gpr.py
@@ -409,6 +409,7 @@ class MAPNEBGPRMover(Motion):
         initial step set up.
         """
         beads_pots, _, _, _ = self.gpr_model.predict_latent_function(self.beads.q)
+        beads_pots = beads_pots + self.optarrays["energy_shift"]
         # print initial geometry and energy of neb path.
         ipi.utils.nebinstool.print_neb_instanton_geo(
             self.options["prefix"] + "_initial_",
@@ -1412,8 +1413,15 @@ class GradientMapper(object):
             self.q_r1, self.V1, self.f_r1, self.h_r1 = ipi.utils.nebinstgprtool.read_reactant_data(reactant1_file)
             self.q_r2, self.V2, self.f_r2, self.h_r2 = ipi.utils.nebinstgprtool.read_reactant_data(reactant2_file) 
 
-            self.q_r1 = self.q_r1[0]
-            self.q_r2 = self.q_r2[0]
+            coord_transformer = self.gpr_model.coordinate_transformer
+            self.q_r1_internal = coord_transformer.get_internal_coordinate_q(self.q_r1)
+            self.f_r1_internal = coord_transformer.transform_cartesian_gradient_to_internal_gradient(self.q_r1, self.f_r1[np.newaxis, :]).squeeze(0)
+            self.h_r1_internal = coord_transformer.transform_cartesian_hessian_to_internal_hessian(self.q_r1, self.f_r1[np.newaxis, :], self.h_r1[np.newaxis,:]).squeeze(0)
+
+            self.q_r2_internal = coord_transformer.get_internal_coordinate_q(self.q_r2)
+            self.f_r2_internal = coord_transformer.transform_cartesian_gradient_to_internal_gradient(self.q_r2, self.f_r2[np.newaxis, :]).squeeze(0)
+            self.h_r2_internal = coord_transformer.transform_cartesian_hessian_to_internal_hessian(self.q_r2, self.f_r2[np.newaxis, :], self.h_r2[np.newaxis, :]).squeeze(0)
+
             self.pot_cutoff = self.gpr_absolute_potential_error_criterion
             self.force_cutoff = self.gpr_absolute_force_error_criterion
 
@@ -1472,11 +1480,30 @@ class GradientMapper(object):
                     r1_distance = np.linalg.norm(test_x[i] - self.q_r1)
                     r2_distance = np.linalg.norm(test_x[i] - self.q_r2)
                     if (r1_distance < r2_distance):
-                        beads_potential[i] = self.V1 + (-self.f_r1) @ (test_x[i] - self.q_r1).T + (test_x[i] - self.q_r1) @ self.h_r1 @ (test_x[i] - self.q_r1).T
-                        beads_forces[i] = self.f_r1 + (-self.h_r1) @ (test_x[i] - self.q_r1)
+                        # Taylor expansion in the internal coordinate.
+                        f, V_shift = ipi.utils.nebinstgprtool.taylor_expansion_subroutine(self.coordinate_transformer,
+                                                                                          test_x[i][np.newaxis, :],
+                                                                                          self.q_r1_internal,
+                                                                                          self.f_r1_internal,
+                                                                                          self.h_r1_internal
+                                                                                          ) 
+                        beads_potential[i] = self.V1 + V_shift 
+                        beads_forces[i] = f 
+                        
+                        # beads_potential[i] = self.V1 + (-self.f_r1) @ (test_x[i] - self.q_r1).T + (test_x[i] - self.q_r1) @ self.h_r1 @ (test_x[i] - self.q_r1).T
+                        # beads_forces[i] = self.f_r1 + (-self.h_r1) @ (test_x[i] - self.q_r1)
                     else:
-                        beads_potential[i] = self.V2 + (-self.f_r2) @ (test_x[i] - self.q_r2).T + (test_x[i] - self.q_r2) @ self.h_r2 @ (test_x[i] - self.q_r2).T 
-                        beads_forces[i] = self.f_r2 + (-self.h_r2) @ (test_x[i] - self.q_r2)
+                        # Taylor expansion in the internal coordinate.
+                        f, V_shift = ipi.utils.nebinstgprtool.taylor_expansion_subroutine(self.coordinate_transformer,
+                                                                    test_x[i][np.newaxis, :],
+                                                                    self.q_r2_internal,
+                                                                    self.f_r2_internal,
+                                                                    self.h_r2_internal
+                                                                    ) 
+                        # beads_potential[i] = self.V2 + (-self.f_r2) @ (test_x[i] - self.q_r2).T + (test_x[i] - self.q_r2) @ self.h_r2 @ (test_x[i] - self.q_r2).T 
+                        # beads_forces[i] = self.f_r2 + (-self.h_r2) @ (test_x[i] - self.q_r2)
+                        beads_potential[i] = self.V2 + V_shift 
+                        beads_forces[i] = f 
 
         # check if ab_initio potential and forces are available.
         # If so, use it and then reset it to zero, so we do not re-use it after we move the bead.
@@ -2374,7 +2401,7 @@ class RP_MAP(object):
         step_num = 0
         
         # shift to move to potential minimum in case the potential is flat in the beginning.
-        r_shift = 1e-4
+        r_shift = 1e-3
 
         total_energy_list = []
         while dr > 0:
@@ -2392,6 +2419,10 @@ class RP_MAP(object):
                 r_distance = old_r_distance + r_shift
                 dr = - dr
                 v_r = 0 
+                # change the initial point.
+                for key, value in zip(data_lists.keys(), [x, v, t, 0, v_r, pot]):
+                    data_lists[key].clear()
+                    data_lists[key].append(value)
                 continue 
 
             # check energy conservation

ipi/utils/hessfasttools.py
@@ -25,6 +25,7 @@ class SelectiveHessianCalculation:
     """
     def __init__(self,
                  train_x: np.ndarray,
+                 grad_x: np.ndarray,
                  coordinate_transformer: non_redundant_coordinate_transformer,
                  rigid_internal_dofs_cutoff: np.ndarray,
                  cross_validation_bool
@@ -56,11 +57,14 @@ class SelectiveHessianCalculation:
         input_dim = train_inputs.shape[1]
         self.input_dim = input_dim 
 
-        # compute the change of training input. 
-        # To be safe, we compute it using two approaches and choose the smaller one among two:
-        train_inputs_change1 = np.abs(sq * (vh @ train_x_change))
-        train_inputs_change2 = np.max(train_inputs, axis= 0) - np.min(train_inputs, axis= 0)
-        train_inputs_change = np.min([train_inputs_change1, train_inputs_change2], axis= 0)
+        internal_coord_type = coordinate_transformer.internal_coord_type
+        if internal_coord_type == "Coulomb":
+            train_inputs_change = np.abs(sq * (vh @ train_x_change))
+        elif internal_coord_type == "bond":
+            train_inputs_change = np.max(train_inputs, axis= 0) - np.min(train_inputs, axis= 0)
+        else:
+            print("Warning: internal coord type unrecognized.")
+            train_inputs_change = np.max(train_inputs, axis= 0) - np.min(train_inputs, axis= 0)
 
         self.train_inputs_change = train_inputs_change 
 
@@ -75,8 +79,29 @@ class SelectiveHessianCalculation:
                 i for i in range(input_dim)
                 if train_inputs_change[i] < rigid_internal_dofs_cutoff
             ]
-        )
+        ).astype(int)
 
+        # choose rigid dofs according to grad criterion.
+        grad_q = (
+            self.coordinate_transformer.transform_cartesian_gradient_to_internal_gradient(
+                train_x, grad_x
+            )
+        )
+        grads_change_cutoff_ratio = 0.1
+        grads_change = np.max(grad_q, axis= 0) - np.min(grad_q, axis= 0)
+        grads_change_cutoff = np.max(grads_change) * grads_change_cutoff_ratio
+        rigid_internal_dofs_with_force_criterion = np.arange(self.input_dim)[grads_change < grads_change_cutoff]
+
+        self.rigid_internal_dofs = np.array(list(
+                                    set(
+                                        np.concatenate([
+                                                self.rigid_internal_dofs, 
+                                                rigid_internal_dofs_with_force_criterion]
+                                                )
+                                        )
+                                        )
+                                    )
+        
         print(f"@hessian calculation: rigid internal dofs: {self.rigid_internal_dofs}")
 
         if len(self.rigid_internal_dofs) != 0:

ipi/utils/nebinstgprtool.py
@@ -5,6 +5,7 @@ Written by Chenghao Zhang & Niri Govind, Pacific Northwest National Laboratory (
 
 import numpy as np
 from gpr.gprtools import GPModelWithDerivativesWrapper
+from gpr.internal.CoulombInternal import non_redundant_coordinate_transformer
 import re
 import os
 from ipi.utils.nebinstool import RK4
@@ -17,6 +18,26 @@ import shutil
 from collections import namedtuple
 import h5py
 from pyquaternion import Quaternion
+
+def taylor_expansion_subroutine(coord_transformer:non_redundant_coordinate_transformer, q, 
+                                q_r_internal, 
+                                f_r_internal, 
+                                h_r_internal):
+    """
+    use taylor expansion in the internal coordinate to compute force at q and potential at q. 
+    q: cartesian coordinate of point. [1, 3 * natoms]
+    q_r: cartesian coordinate of reactant. [1, 3 * natoms]
+    V_r, f_r, h_r : potential, force and hessian. all in cartesian coordinate.
+    """
+    q_internal = coord_transformer.get_internal_coordinate_q(q)
+
+    diff_internal = np.squeeze(q_internal - q_r_internal, axis= 0)
+    f_internal = - h_r_internal @ diff_internal + f_r_internal
+
+    f = coord_transformer.transform_internal_gradient_to_cartesian_gradient(q, f_internal[np.newaxis, :]).squeeze(0)
+    V_shift = (-f_r_internal) @ diff_internal + 0.5 * diff_internal @ h_r_internal @ diff_internal.T
+
+    return f, V_shift 
 
 class force_predictor():
     def __init__(self,
@@ -52,6 +73,15 @@ class force_predictor():
             self.V1 = self.V1 - energy_shift
             self.V2 = self.V2 - energy_shift
 
+            coord_transformer = self.gpr_model.coordinate_transformer
+            self.q_r1_internal = coord_transformer.get_internal_coordinate_q(self.q_r1)
+            self.f_r1_internal = coord_transformer.transform_cartesian_gradient_to_internal_gradient(self.q_r1, self.f_r1[np.newaxis, :]).squeeze(0)
+            self.h_r1_internal = coord_transformer.transform_cartesian_hessian_to_internal_hessian(self.q_r1, self.f_r1[np.newaxis, :], self.h_r1[np.newaxis,:]).squeeze(0)
+
+            self.q_r2_internal = coord_transformer.get_internal_coordinate_q(self.q_r2)
+            self.f_r2_internal = coord_transformer.transform_cartesian_gradient_to_internal_gradient(self.q_r2, self.f_r2[np.newaxis, :]).squeeze(0)
+            self.h_r2_internal = coord_transformer.transform_cartesian_hessian_to_internal_hessian(self.q_r2, self.f_r2[np.newaxis, :], self.h_r2[np.newaxis, :]).squeeze(0)
+
     def predict_force(self, q, r):
         """
         q: cartesian coordinate of beads. shape: [1, 3 * natoms]
@@ -69,16 +99,29 @@ class force_predictor():
                 return f 
             elif r < 0.2:
                 # use Tylor expansion around reactant 1.
-                diff = np.squeeze(q - self.q_r1, axis= 0)
-                f = - self.h_r1 @ diff + self.f_r1
-                V = self.V1 + (-self.f_r1) @ diff + 0.5 * diff @ self.h_r1 @ diff.T
-                V_shift = V - self.V1 
+                # diff = np.squeeze(q - self.q_r1, axis= 0)
+                # f = - self.h_r1 @ diff + self.f_r1
+                # V = self.V1 + (-self.f_r1) @ diff + 0.5 * diff @ self.h_r1 @ diff.T
+                # V_shift = V - self.V1 
+
+                f, V_shift = taylor_expansion_subroutine(self.gpr_model.coordinate_transformer, 
+                                                         q, 
+                                                         self.q_r1_internal, 
+                                                         self.f_r1_internal, 
+                                                         self.h_r1_internal)
             else:
                 # use Tylor expansion around reactant 2
-                diff = np.squeeze(q - self.q_r2, axis= 0)
-                f = - self.h_r2 @ diff + self.f_r2
-                V = self.V2 + (-self.f_r2) @ diff + 0.5 * diff @ self.h_r2 @ diff.T    
-                V_shift = V - self.V2   
+                # diff = np.squeeze(q - self.q_r2, axis= 0)
+                # f = - self.h_r2 @ diff + self.f_r2
+                # V = self.V2 + (-self.f_r2) @ diff + 0.5 * diff @ self.h_r2 @ diff.T    
+                # V_shift = V - self.V2   
+
+                f, V_shift = taylor_expansion_subroutine(self.gpr_model.coordinate_transformer, 
+                                                         q, 
+                                                         self.q_r2_internal, 
+                                                         self.f_r2_internal, 
+                                                         self.h_r2_internal)
+
             f_amplitude = np.linalg.norm(f)
             if f_amplitude < self.fc and V_shift < self.Vc:
                 # when force amplitude and potential is small enough, use Tylor expansion.
@@ -105,15 +148,30 @@ class force_predictor():
                     continue 
                 elif r < 0.2:
                     # use Tylor expansion around reactant 1.
-                    diff = q - self.q_r1[0]
-                    f = - self.h_r1 @ diff + self.f_r1
-                    V = self.V1 + (-self.f_r1) @ diff + 0.5 * diff @ self.h_r1 @ diff.T
-                    V_shift = V - self.V1
+                    # diff = q - self.q_r1[0]
+                    # f = - self.h_r1 @ diff + self.f_r1
+                    # V = self.V1 + (-self.f_r1) @ diff + 0.5 * diff @ self.h_r1 @ diff.T
+                    # V_shift = V - self.V1
+
+                    f, V_shift = taylor_expansion_subroutine(self.gpr_model.coordinate_transformer,
+                                                             np.array([q]), 
+                                                             self.q_r1_internal, 
+                                                             self.f_r1_internal, 
+                                                             self.h_r1_internal)
+                    V = V_shift + self.V1
                 else:
-                    diff = q - self.q_r2[0]
-                    f = - self.h_r2 @ diff + self.f_r2
-                    V = self.V2 + (-self.f_r2) @ diff + 0.5 * diff @ self.h_r2 @ diff.T    
-                    V_shift = V - self.V2
+                    # diff = q - self.q_r2[0]
+                    # f = - self.h_r2 @ diff + self.f_r2
+                    # V = self.V2 + (-self.f_r2) @ diff + 0.5 * diff @ self.h_r2 @ diff.T    
+                    # V_shift = V - self.V2
+
+                    f, V_shift = taylor_expansion_subroutine(self.gpr_model.coordinate_transformer,
+                                                             np.array([q]), 
+                                                             self.q_r2_internal, 
+                                                             self.f_r2_internal, 
+                                                             self.h_r2_internal)
+                    V = V_shift + self.V2 
+
                 f_amplitude = np.linalg.norm(f)
 
                 if f_amplitude < self.fc and V_shift < self.Vc:

tools/py/neb_instanton_postproc.py
@@ -495,8 +495,10 @@ def compute_instanton_rate_or_splitting():
 
     if not quiet:
         print("Diagonalization ... \n\n")
-        # d: eigvalue for mass weighted hessian after deleting trans & rot dof. w: eigenvector, detI: determinant of momentum of inertia
-        m3_for_hessian = np.repeat(m3, repeats= 2, axis= 0)
+        if cal_type == "rate":
+            m3_for_hessian = m3
+        else:
+            m3_for_hessian = np.repeat(m3, repeats= 2, axis= 0) 
         hess_eigval, hess_eigvec, detI = clean_hessian(h, pos, natoms, nbeads, m, m3_for_hessian, asr, mofi=True)  # remove the  translational and rotational modes.
         print("Final lowest 10 frequencies (cm^-1)")
         d10 = np.array2string(
@@ -506,12 +508,11 @@ def compute_instanton_rate_or_splitting():
             formatter={"float_kind": lambda x: "%.2f" % x},
         )
         print(("{}".format(d10)))
+
+        # print conditional number
     
     # print instanton path for half ring polymer
-    if cal_type == "rate":
-        nbeads_to_print = int(nbeads / 2)
-    else:
-        nbeads_to_print = int(nbeads/2) 
+    nbeads_to_print = int(nbeads/2) 
     print_instanton_path(nbeads_to_print, natoms, neb_beads.names, pos, pots)
 
     if  cal_type == "rate":