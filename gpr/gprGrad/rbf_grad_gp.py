add fixed dofs cirterion back to model. We find excluding fixed dofs will improve the stability of uncertainty quantification (posterior var) calculation.
If we include some dofs, the uncertainty of given data point  can still be large even after we add that data point to the model.

gpr/gprGrad/rbf_grad_gp.py
@@ -210,17 +210,17 @@ class GPModelWithDerivatives(gpytorch.models.ExactGP):
             length_scale_min_cutoff = length_scale_min_cutoff * lengthscale_rescale_factor 
             length_scale_max_cutoff = length_scale_max_cutoff * lengthscale_rescale_factor
 
-            if self.FixingDofs is not None:
-                if i == 0:
-                    irrelevant_dofs = fixed_dofs
-                elif i == 1:
-                    irrelevant_dofs = free_moving_dofs
-                else:
-                    irrelevant_dofs = torch.tensor([]) 
-                if len(irrelevant_dofs) > 0:
-                    length_scale[irrelevant_dofs] = irrelevant_lengthscale_ratio * 2 * train_inputs_range[irrelevant_dofs]
-                    length_scale_min_cutoff[irrelevant_dofs] = irrelevant_lengthscale_ratio * train_inputs_range[irrelevant_dofs]
-                    length_scale_max_cutoff[irrelevant_dofs] = irrelevant_lengthscale_ratio * 10 * train_inputs_range[irrelevant_dofs]
+            # if self.FixingDofs is not None:
+            #     if i == 0:
+            #         irrelevant_dofs = fixed_dofs
+            #     elif i == 1:
+            #         irrelevant_dofs = free_moving_dofs
+            #     else:
+            #         irrelevant_dofs = torch.tensor([]) 
+            #     if len(irrelevant_dofs) > 0:
+            #         length_scale[irrelevant_dofs] = irrelevant_lengthscale_ratio * 2 * train_inputs_range[irrelevant_dofs]
+            #         length_scale_min_cutoff[irrelevant_dofs] = irrelevant_lengthscale_ratio * train_inputs_range[irrelevant_dofs]
+            #         length_scale_max_cutoff[irrelevant_dofs] = irrelevant_lengthscale_ratio * 10 * train_inputs_range[irrelevant_dofs]
 
             length_gamma_beta = torch.div(
                 length_gamma_alpha, length_scale
@@ -230,9 +230,6 @@ class GPModelWithDerivatives(gpytorch.models.ExactGP):
             lengthscale_prior = gpytorch.priors.GammaPrior(
                 length_gamma_alpha, length_gamma_beta
             )
-
-            #FIXME: set lengthscale prior to None
-            # lengthscale_prior = None 
 
             lengthscale_constraint = gpytorch.constraints.Interval(
                 length_scale_min_cutoff, length_scale_max_cutoff
@@ -332,6 +329,18 @@ class GPModelWithDerivatives(gpytorch.models.ExactGP):
         force_noises = task_noises_std[1:]
 
         return V_noises, force_noises
+
+    def compute_covar_matrix_condition_number(self):
+        """
+        compute the condition number of the covariance matrix K.
+        We include the noise in the likelihood.
+        """
+        prediction_strategy = self.prediction_strategy
+        covar_matrix = prediction_strategy.lik_train_train_covar
+        covar_matrix = covar_matrix.to_dense()
+        condition_number = torch.linalg.cond(covar_matrix)
+        condition_number = condition_number.cpu().detach().numpy()
+        return condition_number
 
     # __call__ function in Gpytorch code. 
     # We need to change the prediction strategy to use pseudo-inverse when inverse the covariance matrix. 

gpr/gprtools.py
@@ -278,14 +278,15 @@ class FixInternalDofs(object):
 
         
         # code that use the change in forces as criterion to select dofs to include in GPR model.
-        # grad_q = train_targets[:, 1:]
-        # grad_q_change = np.max(grad_q, axis = 0) - np.min(grad_q, axis= 0)
-        # grad_q_change_cutoff = np.max(grad_q_change) / np.power(10.0, 3)
+        grad_q = train_targets[:, 1:]
+        grad_q_change = np.max(grad_q, axis = 0) - np.min(grad_q, axis= 0)
+        grad_q_change_cutoff = np.max(grad_q_change) / np.power(10.0, 2)
 
-        # fixed_internal_dofs_grad_criterion = np.arange(self.input_dim)[ grad_q_change <= grad_q_change_cutoff ]
+        fixed_internal_dofs_grad_criterion = np.arange(self.input_dim)[ grad_q_change <= grad_q_change_cutoff ]
         
-        # self.fixed_internal_dofs = np.union1d(fixed_internal_dofs_inputs_change_criterion, fixed_internal_dofs_grad_criterion).astype(int)
-        self.fixed_internal_dofs = fixed_internal_dofs_inputs_change_criterion.astype(int)
+        self.fixed_internal_dofs = np.union1d(fixed_internal_dofs_inputs_change_criterion, fixed_internal_dofs_grad_criterion).astype(int)
+
+        # self.fixed_internal_dofs = fixed_internal_dofs_inputs_change_criterion.astype(int)
 
         # load the fixed internal dofs from the folder.
         if gpr_fixed_internal_dofs is not None:
@@ -657,9 +658,9 @@ class GPModelWithDerivativesWrapper:
         # training outputs in internal coordinates q. (V, dV/dq)
         self.normalized_train_targets = train_targets  
 
-        # train_inputs, train_targets, likelihood_noise_variance = self.fix_internal_dofs(
-        #     train_inputs, train_targets, likelihood_noise_variance
-        # )
+        train_inputs, train_targets, likelihood_noise_variance = self.fix_internal_dofs(
+            train_inputs, train_targets, likelihood_noise_variance
+        )
 
         # ------- transform input from numpy array to torch.tensor -----------
         (train_inputs, train_targets) = map(
@@ -832,11 +833,11 @@ class GPModelWithDerivativesWrapper:
         )
 
         # attach test_mean and test_var (0) of fixed dofs
-        # test_mean, test_covar_matrix = (
-        #     self.FixingDofs.transform_from_free_moving_dofs_to_full_dofs(
-        #         test_mean, test_covar_matrix
-        #     )
-        # )
+        test_mean, test_covar_matrix = (
+            self.FixingDofs.transform_from_free_moving_dofs_to_full_dofs(
+                test_mean, test_covar_matrix
+            )
+        )
 
         # inverse the normalization procedure for mean value and variance.
         test_covar_matrix_q = self.Normalizer.inverse_normalize_noise_covar_matrix(
@@ -933,23 +934,23 @@ class GPModelWithDerivativesWrapper:
         )
 
         # fix certain dofs from input and targets, not including it in our gpr model.
-        # new_train_inputs, new_train_targets = (
-        #     self.FixingDofs.transform_training_data_to_free_moving_dofs(
-        #         new_train_inputs, new_train_targets
-        #     )
-        # )
+        new_normalized_free_moving_train_inputs, new_free_moving_train_targets = (
+            self.FixingDofs.transform_training_data_to_free_moving_dofs(
+                new_normalized_train_inputs, new_train_targets
+            )
+        )
 
         # transform numpy array into tensor.
-        (new_train_inputs_tensor, new_normalized_train_inputs_tensor, new_train_targets_tensor) = map(
+        (new_normalized_free_moving_train_inputs_tensor, new_free_moving_train_targets_tensor) = map(
             lambda x: torch.from_numpy(x).to(device= self.device, dtype=torch.float64), 
-            (new_train_inputs, new_normalized_train_inputs, new_train_targets)
+            (new_normalized_free_moving_train_inputs, new_free_moving_train_targets)
         )
 
         # we only add new training data if they are not too close to each other.
         filtered_new_train_inputs_index = update_model_with_new_data(
             self.gpr_model,
-            new_normalized_train_inputs_tensor,
-            new_train_targets_tensor,
+            new_normalized_free_moving_train_inputs_tensor,
+            new_free_moving_train_targets_tensor,
             distance_cutoff,
             train_bool
         )
@@ -1105,6 +1106,11 @@ class GPModelWithDerivativesWrapper:
         fixed_internal_dofs = np.copy(self.FixingDofs.fixed_internal_dofs)
         return fixed_internal_dofs
 
+    def compute_covar_matrix_condition_number(self):
+        """
+        compute the condition number of the covariance matrix K.
+        """
+        return self.gpr_model.compute_covar_matrix_condition_number()
 
     def get_free_moving_internal_coordinate(self, beads_x):
         """
@@ -1121,11 +1127,11 @@ class GPModelWithDerivativesWrapper:
             )
         )
 
-        # beads_internal_coordinate = (
-        #     self.FixingDofs.transform_training_inputs_to_free_moving_dofs(
-        #         beads_internal_coordinate
-        #     )
-        # )
+        beads_internal_coordinate = (
+            self.FixingDofs.transform_training_inputs_to_free_moving_dofs(
+                beads_internal_coordinate
+            )
+        )
 
         return beads_internal_coordinate
 

gpr_script/gpr_interface.py
@@ -389,6 +389,7 @@ class GPRForceMapper(object):
             _, _, _, var_grad_x_trace_list = self.gpr_model.predict_latent_function(beads_q)
 
             force_uncertainty = np.sqrt(var_grad_x_trace_list)
+            print("@update GPR model. Force uncertainty for all beads: " + str(force_uncertainty))
 
             large_uncertainty_bool = (force_uncertainty > force_uncertainty_cutoff)
             large_uncertainty_bead_index = np.arange(nbeads)[large_uncertainty_bool]
@@ -403,8 +404,10 @@ class GPRForceMapper(object):
         Also compute the ab initio forces for the new data.
         """
         # ML learned forces.
-        _, beads_grads, _, _ = self.gpr_model.predict_latent_function(new_training_x)
+        _, beads_grads, _, beads_grads_var = self.gpr_model.predict_latent_function(new_training_x)
         gpr_beads_forces = - beads_grads
+        beads_grads_uncertainty = np.sqrt(beads_grads_var)
+        self.force_uncertainty_list = beads_grads_uncertainty
         
         # ab initio forces.
         bead_number = new_training_x.shape[0]
@@ -438,6 +441,8 @@ class GPRForceMapper(object):
         """
         distance_cutoff = self.motion.options["distance_cutoff_for_training_data"]
         train_grad_model_bool = self.motion.options["train_grad_model_bool"]
+        # FIXME: See if we fix the gpr model parameter during data point update, will the issue be resolved.
+        train_grad_model_bool= False
 
         new_shifted_pots = new_ab_initio_pots - self.energy_shift
         new_ab_initio_grads = - new_ab_initio_forces
@@ -455,25 +460,27 @@ class GPRForceMapper(object):
         compute the force error after update the gpr model.
         """
          # ML learned forces.
-        _, beads_grads, _, _ = self.gpr_model.predict_latent_function(new_training_x)
+        _, beads_grads, _, beads_grads_var = self.gpr_model.predict_latent_function(new_training_x)
         gpr_beads_forces = - beads_grads
-
+        beads_force_uncertainty = np.sqrt(beads_grads_var)
+        self.force_uncertainty_after_update_list = beads_force_uncertainty
+        
         force_diff = gpr_beads_forces - new_ab_initio_forces
         self.force_diff_amplitude_after_update_list = np.linalg.norm(force_diff, axis= 1)
         self.force_diff_ratio_after_update_list = (
             self.force_diff_amplitude_after_update_list / self.ab_initio_force_amplitude_list
         )
 
-        # check the uncertainty of force for the updated potential.
-        # increase the gpr_force_uncertainty criterion if it is not met after we have updated the pot.
-        beads_q = self.motion.beads.q 
-        _, _, _, var_grad_x_uncertainty = self.gpr_model.predict_latent_function(beads_q)
-        max_std_grad_x_uncertainty = np.max(np.sqrt(var_grad_x_uncertainty))
-        if max_std_grad_x_uncertainty > self.motion.optarrays["gpr_force_uncertainty_criterion"]:
-            print("@Warning: The uncertainty of gpr prediction is still higher than cutoff criterion after update the model.")
-            print(f"max std force uncertainty: {max_std_grad_x_uncertainty}")
-            print(f"The force uncertainty criterion will be increased to {max_std_grad_x_uncertainty}")
-            self.motion.optarrays["gpr_force_uncertainty_criterion"] = max_std_grad_x_uncertainty
+        # # check the uncertainty of force for the updated potential.
+        # # increase the gpr_force_uncertainty criterion if it is not met after we have updated the pot.
+        # beads_q = self.motion.beads.q 
+        # _, _, _, var_grad_x_uncertainty = self.gpr_model.predict_latent_function(beads_q)
+        # max_std_grad_x_uncertainty = np.max(np.sqrt(var_grad_x_uncertainty))
+        # if max_std_grad_x_uncertainty > self.motion.optarrays["gpr_force_uncertainty_criterion"]:
+        #     print("@Warning: The uncertainty of gpr prediction is still higher than cutoff criterion after update the model.")
+        #     print(f"max std force uncertainty: {max_std_grad_x_uncertainty}")
+        #     print(f"The force uncertainty criterion will be increased to {max_std_grad_x_uncertainty}")
+        #     self.motion.optarrays["gpr_force_uncertainty_criterion"] = max_std_grad_x_uncertainty
 
     def output_force_error_info(self, step):
         """
@@ -497,12 +504,13 @@ class GPRForceMapper(object):
             print(
                 "@Outerloop Exit info: |f_GPR -f| :" + str(self.force_diff_amplitude_list)
             )
-
+            print("@Outerloop Exit info: |f_GPR| uncertainty given by GPR model:" + str(self.force_uncertainty_list))
             print("After update:")
             print("@Outerloop Exit info: |f_GPR -f|/|f|:" + str(self.force_diff_ratio_after_update_list))
             print(
                 "@Outerloop Exit info: |f_GPR -f| :" + str(self.force_diff_amplitude_after_update_list)
             )
+            print("@Outlerloop Exit info: |f_GPR| uncertainty given by GPR model:" + str(self.force_uncertainty_after_update_list))
 
             print("Finish Outerloop: " + str(step))
             print("\n")

ipi/engine/motion/neb_instanton_gpr.py
@@ -103,7 +103,7 @@ class MAPNEBGPRMover(Motion):
         gpr_force_uncertainty_criterion = 0.001,
         gpr_trust_region=0.1,
         minimum_trust_region= 0.1,
-        distance_cutoff_for_training_data= 0.1,
+        distance_cutoff_for_training_data= 0.01,
         gpr_kernel_outputscale=np.ones(0, float),
         gpr_kernel_outputscale_constraint = {
             "min": 0.0,